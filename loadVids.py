be5dc3dc7dec6c636d879c773c195df2427d6fbf
Robert Timpe <rob.timpe@gmail.com>
2015-11-13 22:45:53 -0800

    Tweak params and add script for loading into vatic

---
+++ loadVids.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import shutil
+import subprocess
+
+def main():
+	for f in os.listdir(sys.argv[1]):
+		if f.startswith('video'):
+			parts = f.split()
+			newName = '_'.join(parts)
+			path = sys.argv[2] + newName
+			shutil.copyfile(sys.argv[1] + f, path)
+			iden = f.split('.')[0]
+			outputPath = sys.argv[2] + iden
+			os.mkdir(outputPath)
+			subprocess.call(['turkic', 'extract', path, outputPath])
+
+			subprocess.call(['turkic', 'load', iden, outputPath, 'arm'])
+
+main()
